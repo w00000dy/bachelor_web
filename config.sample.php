76b179407f56f26122d8d042f6a07bcb0876261a
WoodyLetsCode <27882680+WoodyLetsCode@users.noreply.github.com>
2024-08-11 15:19:46 +0200

    Update database configuration file template

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,9 @@
 <?php
+/*  DATABASE CONFIGURATION 
+    Fill in the following variables with your database login information.
+    Rename this file to config.php
+*/
+
 $dbHost = "localhost";
 $database = "";
 $dbUsername = "";
