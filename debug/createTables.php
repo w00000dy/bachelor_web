ded0e1ea807c42c2b6cb57bddc88cc76402c0447
WoodyLetsCode <27882680+WoodyLetsCode@users.noreply.github.com>
2024-08-11 16:07:34 +0200

    added createTables.php

---
+++ debug/createTables.php
@@ -0,0 +1,83 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+header('Content-Type: text/plain');
+require_once '../config.php';
+
+
+// Create connection
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $database);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table Participants
+$sql = "CREATE TABLE Participants (
+id CHAR(13) NOT NULL PRIMARY KEY,
+reg_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Participants created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error . "\n";
+}
+
+// sql to create table electronic_devices_main_application
+$sql = "CREATE TABLE electronic_devices_main_application (
+id CHAR(13) NOT NULL PRIMARY KEY REFERENCES Participants(id),
+social_media BOOLEAN NOT NULL,
+communication BOOLEAN NOT NULL,
+entertainment BOOLEAN NOT NULL,
+work_and_productivity BOOLEAN NOT NULL,
+information_search BOOLEAN NOT NULL,
+organization_and_planning BOOLEAN NOT NULL,
+navigation_and_maps BOOLEAN NOT NULL,
+health_and_fitness BOOLEAN NOT NULL,
+smart_home BOOLEAN NOT NULL)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table electronic_devices_main_application created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error . "\n";
+}
+
+// sql to create table bt_devices
+$sql = "CREATE TABLE bt_devices (
+    id CHAR(13) NOT NULL PRIMARY KEY REFERENCES Participants(id),
+    smartwatches_fitness_trackers BOOLEAN NOT NULL,
+    headphones BOOLEAN NOT NULL,
+    keyboard_mouse BOOLEAN NOT NULL,
+    others BOOLEAN NOT NULL)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table bt_devices created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error . "\n";
+}
+
+// sql to create table Survey
+$sql = "CREATE TABLE Survey (
+id CHAR(13) NOT NULL PRIMARY KEY REFERENCES Participants(id),
+age TINYINT UNSIGNED,
+gender BOOLEAN,
+num_electronic_devices TINYINT UNSIGNED,
+regularly_tablet_laptop_use BOOLEAN,
+electronic_devices_main_application INT,
+technical_experience TINYINT UNSIGNED,
+encountered_problems BOOLEAN,
+bt_pairing_problems BOOLEAN,
+problems_explaination TEXT,
+situation_already_encountered TINYINT UNSIGNED,
+know_pairing_code BOOLEAN,
+improvements TEXT,
+additional_thoughts TEXT)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Survey created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error . "\n";
+}
+
+$conn->close();
