dbba4104c1d06589c614fc70b1a711620d1231b9
WoodyLetsCode <27882680+WoodyLetsCode@users.noreply.github.com>
2024-08-13 21:49:58 +0200

    added status and fake loading screen to phone page

--- sendFile/phone/phone.js
+++ sendFile/phone/phone.js
@@ -1,14 +1,76 @@
-formFile.addEventListener('submit', function (e) {
-	e.preventDefault();
+var fileUploaded = false;
+var connectionStatus;
 
-    fetch('file.php').then(response => {
+fetchStatus();
+
+formFile.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    fetch('status.php').then(response => {
         return response.text();
     }
     ).then(data => {
         console.log(data);
-        if (data === 'Success') {
-            alert('File sent successfully!\nTask completed!');
-            window.location.href = '/thanks/';
+        if (data === '3') {
+            fileUploaded = true;
+            btnSubmit.disabled = true;
+            fakeUpload();
         }
     });
 });
+
+async function fakeUpload() {
+    progress.hidden = false;
+    let random = Math.floor(Math.random() * 5) + 8;
+    progress.value += random;
+    if (progress.value < progress.max) {
+        setTimeout(fakeUpload, 10);
+    }
+    else {
+        progress.hidden = true;
+        btnSubmit.disabled = false;
+        fileUploaded = true;
+        fileUploadedText.hidden = false;
+        notConnected.hidden = true;
+        connecting.hidden = true;
+        connected.hidden = true;
+        alert(langData['fileSuccess']);
+        progress.value = 0;
+    }
+}
+
+async function fetchStatus() {
+    let response;
+    let data;
+    try {
+        response = await fetch('status.php');
+        data = await response.text();
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+    connectionStatus = data;
+    if (fileUploaded) {
+        return;
+    }
+    switch (data) {
+        case '5':
+            notConnected.hidden = true;
+            connecting.hidden = false;
+            connected.hidden = true;
+            btnSubmit.disabled = true;
+            break;
+        case '3':
+            notConnected.hidden = true;
+            connecting.hidden = true;
+            connected.hidden = false;
+            btnSubmit.disabled = false;
+            break;
+        default:
+            notConnected.hidden = false;
+            connecting.hidden = true;
+            connected.hidden = true;
+            btnSubmit.disabled = true;
+    }
+    setTimeout(fetchStatus, 1000);
+}
--- sendFile/phone/status.php
+++ sendFile/phone/status.php
@@ -4,13 +4,10 @@ require_once '../../mysql.php';
 $sql = "SELECT * FROM `action_log` ORDER BY `action_log`.`date` DESC ";
 $result = $conn->query($sql);
 
-// check if in first row action = 3
+header('Content-Type: text/plain');
+
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    if ($row['action'] == 3) {
-        echo "Success";
-        return;
-    }
+    echo $row['action'];
+    return;
 }
-
-echo "Not connected";
